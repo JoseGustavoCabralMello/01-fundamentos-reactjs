70af373bdd2df6b78f581aabee232d63405c76d7
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2024-09-15 19:40:47 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
 import { Post } from "./Post"
+
+import styles from "./App.module.css"
 
 import "./global.css"
 
@@ -7,16 +10,21 @@ export function App() {
   return (
     <div>
       <Header/>
-      <h1>Hello World!</h1>
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident laboriosam cum consequuntur amet molestiae suscipit veritatis, recusandae eveniet obcaecati? Totam ipsam, dolorem soluta in inventore odit mollitia veniam! Distinctio, harum."
-      />
-      <Post
-        author="Gustavo Guanabara"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident laboriosam cum consequuntur amet molestiae suscipit veritatis, recusandae eveniet obcaecati? Totam ipsam, dolorem soluta in inventore odit mollitia veniam! Distinctio, harum."
-      />
-      <Post/>
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident laboriosam cum consequuntur amet molestiae suscipit veritatis, recusandae eveniet obcaecati? Totam ipsam, dolorem soluta in inventore odit mollitia veniam! Distinctio, harum."
+          />
+          <Post
+            author="Gustavo Guanabara"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident laboriosam cum consequuntur amet molestiae suscipit veritatis, recusandae eveniet obcaecati? Totam ipsam, dolorem soluta in inventore odit mollitia veniam! Distinctio, harum."
+          />
+        </main>
+      </div>
     </div>
   )
 }
