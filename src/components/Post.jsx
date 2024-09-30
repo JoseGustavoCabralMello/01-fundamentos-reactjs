babe8ef52bfebb5ae94f44de4afafc9527cd66f5
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2024-09-30 07:41:21 -0300

    Os motores do React - Estado (useState)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,9 +3,17 @@ import ptBR from "date-fns/locale/pt-BR"
 
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
+import { useState } from "react";
+
 import styles from "./Post.module.css";
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    1,
+    2,
+    3,
+  ])
+
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
   })
@@ -14,6 +22,12 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true,
   })
+
+  function handleCreateNewComment() {
+    event.preventDefault()
+
+    setComments([...comments, comments.length + 1]);
+  }
 
   return(
     <article className={styles.post}>
@@ -41,7 +55,7 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea
@@ -54,9 +68,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment />
+        })}
       </div>
     </article>
   )
