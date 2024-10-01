210f18db2695e6fa8a7cf4bd01cba6ff32069358
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2024-10-01 08:25:40 -0300

    Os motores do React - Inserindo comentários (Programação declarativa)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,10 +9,10 @@ import styles from "./Post.module.css";
 
 export function Post({ author, publishedAt, content }) {
   const [comments, setComments] = useState([
-    1,
-    2,
-    3,
+    "Post muito bacana, hein?!"
   ])
+
+  const [newCommentText, setNewCommentText] = useState("")
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
@@ -24,9 +24,14 @@ export function Post({ author, publishedAt, content }) {
   })
 
   function handleCreateNewComment() {
-    event.preventDefault()
+    event.preventDefault();
 
-    setComments([...comments, comments.length + 1]);
+    setComments([...comments, newCommentText]);
+    setNewCommentText("");
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
   }
 
   return(
@@ -59,7 +64,10 @@ export function Post({ author, publishedAt, content }) {
         <strong>Deixe seu feedback</strong>
 
         <textarea
+          name="comment"
           placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
         />
 
         <footer>
@@ -69,7 +77,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment />
+          return <Comment content={comment}/>
         })}
       </div>
     </article>
